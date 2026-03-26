bf8efe3440e3a68f897035984dfed3541405dff1
prince476-web <princemovaliya467@gmail.com>
2026-03-26 18:25:13 +0530

    okoko

--- src/model/categoryes.model.js
+++ src/model/categoryes.model.js
@@ -11,7 +11,7 @@ const categorySchema = new mongoose.Schema(
             type: String
         },
         category_img: {
-            public_id: {
+            public_id: {   
                 type: String,
             },
             url: {
