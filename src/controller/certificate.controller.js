e32563954f50d8026b53cdb70d654ff104cd0115
prince476-web <princemovaliya467@gmail.com>
2026-06-08 16:33:34 +0530

    update certificate name

--- src/controller/certificate.controller.js
+++ src/controller/certificate.controller.js
@@ -1,5 +1,4 @@
-const certificateModel = require('../model/Certificate.model');
-const secrionsModel = require('../model/Certificate.model');
+const certificateModel = require('../model/certificate.model');
 const coursesModel = require('../model/course.model');
 const userModel = require('../model/users.model');
 const createCertificate = require('../service/certificate');
@@ -39,110 +38,110 @@ const generateCertificate = async (req, res) => {
     res.status(200).json({ sucess: true, data: pdf, message: 'pdf Generate sucessfully' })
 
 };
-const getAllSection = async (req, res) => {
-    console.log('terms Routes');
-    try {
-        const Certificate = await secrionsModel.find()
+// const getAllSection = async (req, res) => {
+//     console.log('terms Routes');
+//     try {
+//         const Certificate = await secrionsModel.find()
 
-        console.log(Certificate);
+//         console.log(Certificate);
 
-        res.status(200).json({ sucess: true, data: Certificate, message: 'get sucessfully' })
+//         res.status(200).json({ sucess: true, data: Certificate, message: 'get sucessfully' })
 
-    } catch (error) {
-        res.status(500).json({ sucess: false, data: [], message: 'getAll Certificate error' + error.message })
-    }
+//     } catch (error) {
+//         res.status(500).json({ sucess: false, data: [], message: 'getAll Certificate error' + error.message })
+//     }
 
-}
+// }
 
-const addSection = async (req, res) => {
-    try {
-        console.log("req.body", req.body);
+// const addSection = async (req, res) => {
+//     try {
+//         console.log("req.body", req.body);
 
-        const Certificate = await secrionsModel.create(req.body)
+//         const Certificate = await secrionsModel.create(req.body)
 
-        res.status(200).json({
-            success: true,
-            message: "Certificate added successfully",
-            data: Certificate
-        });
-
-
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            data: [],
-            message: 'add Certificate error' + error.message
-        });
-    }
-};
-
-const updateSection = async (req, res) => {
-    try {
-        console.log("id:", req.params.id);
-        console.log("body:", req.body);
-
-        const sectionData = await secrionsModel.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true }
-        );
+//         res.status(200).json({
+//             success: true,
+//             message: "Certificate added successfully",
+//             data: Certificate
+//         });
 
 
-        if (!sectionData) {
 
-            return res.status(404).json({ data: null, message: 'Certificate not updated' });
-        }
+//     } catch (error) {
+//         res.status(500).json({
+//             success: false,
+//             data: [],
+//             message: 'add Certificate error' + error.message
+//         });
+//     }
+// };
 
-        res.status(200).json({
-            success: true,
-            message: "Certificate update successfully",
-            data: sectionData
-        });
+// const updateSection = async (req, res) => {
+//     try {
+//         console.log("id:", req.params.id);
+//         console.log("body:", req.body);
 
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            data: [],
-            message: 'update Certificate error ' + error.message
-        });
-    }
-}
-
-const deleteSection = async (req, res) => {
-    try {
-        console.log("id:", req.params.id);
+//         const sectionData = await secrionsModel.findByIdAndUpdate(
+//             req.params.id,
+//             req.body,
+//             { new: true }
+//         );
 
 
-        const sectionData = await secrionsModel.findByIdAndDelete(req.params.id)
+//         if (!sectionData) {
 
-        if (!sectionData) {
+//             return res.status(404).json({ data: null, message: 'Certificate not updated' });
+//         }
 
-            return res.status(404).json({ data: null, message: 'Certificate not deleted' });
-        }
-        res.status(200).json({
-            success: true,
-            message: "Certificate delete successfully",
-            data: null
-        });
+//         res.status(200).json({
+//             success: true,
+//             message: "Certificate update successfully",
+//             data: sectionData
+//         });
+
+//     } catch (error) {
+//         res.status(500).json({
+//             success: false,
+//             data: [],
+//             message: 'update Certificate error ' + error.message
+//         });
+//     }
+// }
+
+// const deleteSection = async (req, res) => {
+//     try {
+//         console.log("id:", req.params.id);
 
 
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            data: [],
-            message: 'delete Certificate  Internal Server Error ' + error.message
-        });
-    }
+//         const sectionData = await secrionsModel.findByIdAndDelete(req.params.id)
 
-}
+//         if (!sectionData) {
+
+//             return res.status(404).json({ data: null, message: 'Certificate not deleted' });
+//         }
+//         res.status(200).json({
+//             success: true,
+//             message: "Certificate delete successfully",
+//             data: null
+//         });
+
+
+//     } catch (error) {
+//         res.status(500).json({
+//             success: false,
+//             data: [],
+//             message: 'delete Certificate  Internal Server Error ' + error.message
+//         });
+//     }
+
+// }
 
 
 module.exports = {
-    getAllSection,
-    addSection,
-    updateSection,
-    deleteSection,
+    // getAllSection,
+    // addSection,
+    // updateSection,
+    // deleteSection,
     generateCertificate
 }
 
